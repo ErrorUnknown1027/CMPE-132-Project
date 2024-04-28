18ab7b8383ba686f441f8168d09aedbee057224d
KleinS <freaKENamazing18@gmail.com>
2024-04-28 00:16:51 -0700

    made templates, temp login and signup pages

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == '__main__': ##only if we run this file
-    app.run(debug=True) ##anytime we change the python code, rerun the webserver
+if __name__ == '__main__': ##   only if we run this file
+    app.run(debug=True) ##  anytime we change the python code, rerun the webserver
+    
